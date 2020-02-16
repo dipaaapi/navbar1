0e69a517f60abcb75d7d0a33bdab2349383e04f9
John Edward Cabrera <dipaaapi8@gmail.com>
2020-02-17 05:32:55 +0800

    profile edited and new image added for profile

--- profile.php
+++ profile.php
@@ -2,11 +2,13 @@
 <body>
 	<?php include ('navbar.php') ?>
 	<div class="content" id="content">
-		<div class="p1">
+		<div class="p1-profile">
 			<h1 class="content-title">John Edward "Janjan" M. Cabrera</h1>
-			<div class="imgback">
-				<img class="profile-img" src="img/john.jpg" alt="my image">
-			</div>
+			<a class="img-a" href="#">
+				<div class="imgback">
+					<img class="profile-img" src="img/john.jpg" alt="my image">
+				</div>
+			</a>
 			<p class="para">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ut consequat metus. Nam dictum luctus metus vel mattis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Morbi at mi ultrices, posuere leo id, hendrerit dui. Sed eget feugiat mi, eu vehicula ante. Aliquam a efficitur sem. Integer porttitor ligula vitae iaculis tristique. Sed ornare viverra dui, quis convallis elit convallis in. Vestibulum euismod nulla enim, ut dignissim turpis fermentum vitae. Vestibulum cursus mi ac lorem rutrum, id maximus orci consectetur. Proin finibus scelerisque euismod. Sed et lacinia purus.</p>
 		</div>
 		<?php include ('footer.php') ?>
